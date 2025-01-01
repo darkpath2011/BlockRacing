87c7ad412152d7eea1bc91590e47092f215212d1
Leng <110669856+xiaoshaziYA@users.noreply.github.com>
2025-01-01 11:29:28 +0800

    使用menumanager处理回调函数等

--- src/main/java/top/darkpath2011/blockRacing/object/Menu.java
+++ src/main/java/top/darkpath2011/blockRacing/object/Menu.java
@@ -3,24 +3,17 @@ package top.darkpath2011.blockRacing.object;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.metadata.FixedMetadataValue;
-import top.darkpath2011.blockRacing.BlockRacing;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
 
-/**
- * @Author: ShaoqingG
- * @Created: 2024/12/31 18:59
- * WARN: Please refrain from casually passing judgment on the code. The quality of the code is within the scope of responsibility of the developers themselves. If you think it's what is called "messy codebase" (the term "shit mountain" is rather vulgar), we sincerely welcome you to fork this project, and then submit a pull request (PR) to the project and optimize the relevant code. We will be extremely grateful for that.
- * However, if you merely think the code is poor but are unable to come up with an optimization plan, then please restrain the urge to comment on others' code. This is the most basic rule of courtesy. The respect that developers have for you is based on the respect you show to the developers. This is also a requirement of basic moral cultivation. Meanwhile, we should always keep in mind the core socialist values: prosperity, democracy, civility, harmony; actively advocate freedom, equality, justice, the rule of law; and vigorously promote patriotism, dedication, integrity, and friendship.
- */
 @Getter
 public class Menu {
     private final String title;
@@ -49,5 +42,6 @@ public class Menu {
 
     public void open(Player player) {
         player.openInventory(inventory);
+        player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_AMBIENT, 1.0f, 1.0f);
     }
 }
